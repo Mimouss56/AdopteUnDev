0b4a3ab75a5bc290256b15d055269d612fb0af62
ewen <ewenlelf@gmail.com>
2024-01-25 11:38:49 +0100

    Router prepared

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import {BrowserRoute, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 
 
 function App() {
 
   
 	return (
-    <BrowserRoute>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />}></Route>
+        <Route path="/" element={<Annonce />}></Route>
+        {/* Redirect to Error page*/}
+        <Route path="/error" element={<Error />}></Route>
+        <Route path="*" element={<Navigate to="/error" />}></Route>
       </Routes>
-    </BrowserRoute>
+    </BrowserRouter>
   )
 }
 
