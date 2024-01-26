119921ef9b5dbdf8e279026de71a40bd143dcfef
S-REN-POL1 <S-REN-POL1@open-groupe.com>
2024-01-26 13:37:07 +0100

    route /me

--- app/routers/index.js
+++ app/routers/index.js
@@ -15,6 +15,7 @@ router.use('/tree', treeRouter);
 router.use('/degree', degreeRouter);
 router.use('/offer', offerRouter);
 router.use('/task', taskRouter);
+router.use('/me', require('./me.router'));
 
 router.get('/*', (req, res) => {
   // Modification ici pour prendre en compte les sous-dossiers
